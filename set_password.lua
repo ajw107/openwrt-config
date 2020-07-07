57edf11e118fffd2cdc89f4e1a7ec13a91fb551b
Alex Wood <github@alex-wood.org.uk>
2020-07-07 16:35:10 +0100

    Initial Commit

---
+++ set_password.lua
@@ -0,0 +1,10 @@
+local sys = require "luci.sys"
+
+function set_password(user,pass)
+  sys.user.setpasswd(user,pass)
+ print(0)
+ return
+end
+
+if #arg == 2 then set_password(...) else print(1) end
+
